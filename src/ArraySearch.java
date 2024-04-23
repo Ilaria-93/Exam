8482838071208a4f87f8d5ec8bb42f35c8611213
LorenzoRappuoli <lorenzo.rappuoli@hotmail.com>
2024-04-23 15:23:23 +0200

    first commit

---
+++ src/ArraySearch.java
@@ -0,0 +1,23 @@
+public class ArraySearch {
+    //private int[] arrayDiInteri = {1, 6, 13, 22, 35};
+
+    public boolean cercaNumeroSpecifico(int[] array, int x) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == x) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int trovaPosizioneIndice (int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/* **Ricerca Elemento Array:**
+   - Crea una classe `ArraySearch` con un attributo array di interi.
+   - Implementa un metodo per cercare un numero specifico
+   nell'array e stampare se è presente o meno.
+   - Implementa un metodo per trovare e stampare la posizione
+   (indice) del primo elemento pari presente nell'array.
+   - Utilizza la classe e la funzione nel main
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        ArraySearch search = new ArraySearch();
+        int[] arrayDiInteri = {1, 6, 13, 22, 35};
+        System.out.println("Il numero specifico nell'array è presente: " + search.cercaNumeroSpecifico(arrayDiInteri,3));
+        System.out.println("La posizione del primo elemento pari è: " + search.trovaPosizioneIndice(arrayDiInteri));
+    }
+}
